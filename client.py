039932df9d7d954fa4fe0f38837fe6c0c64e8ebc
Abdulai00 <abdulai.b0001@gmail.com>
2024-09-05 20:53:47 -0400

    A basic prototype of what I want to make iterations are sure to follow

--- client.py
+++ client.py
@@ -1,10 +1,22 @@
 import socket
 
-HEADERSIZE = 10
+HEADERSIZE = 10  # Assuming 10 bytes are used for the message length header
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(),1234))
+s.connect((socket.gethostname(), 1234))
 
+testString = (
+    "GET http://example.com/ HTTP/1.1\r\n"
+    "Host: example.com\r\n"
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36\r\n"
+    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\n"
+    "Accept-Language: en-US,en;q=0.5\r\n"
+    "Connection: keep-alive\r\n\r\n"
+)
+
+s.send(testString.encode())
+
+print("Sent the message")
 while True:
     full_msg = ""
     new_msg = True
@@ -12,16 +24,18 @@ while True:
     while True:
         msg = s.recv(16)
         if new_msg:
-            print(f"new message length : {msg[:HEADERSIZE]}")
+            print(f"New message length: {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE])
+            print(msglen)
             new_msg = False
-        
+
         full_msg += msg.decode()
 
         if len(full_msg) - HEADERSIZE == msglen:
-            print("recevied full message")
+            print("Received full message")
             print(full_msg[HEADERSIZE:])
-            new_msg = True
-            full_msg = ''
+            break  # Exit after receiving the full message
+    break
 
-    print(full_msg)
+s.close()
+print("Disconnected")
--- server.py
+++ server.py
@@ -3,27 +3,8 @@ import ssl
 import threading
 import requests
 
-HEADERSIZE = 64
+HEADERSIZE = 10
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(),1234))
-
-
-def handle_client(commS,address):
-    print(f"New connection to {address}")
-    connected = True
-    while connected:
-        msg_length = commS.recv(HEADERSIZE).decode('utf-8')
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = commS.recv(msg_length).decode('utf-8')
-            if len(msg) == 0:
-                connected = False
-
-            print("the message from {address} is {msg}")
-
-    commS.close()
-    
 class HttpObj:
         def __init__(self,method,uri,version) -> None:
             self.type = method
@@ -31,62 +12,100 @@ class HttpObj:
             self.version = version
             self.headers_dict = None
             self.body = None
-            
-def parse_http(msg):
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(),1234))
+
+def handle_client(commS, address):
+    print(f"New connection to {address}")
+    connected = True
+    data = ""
     
-     
-    if "\r\n" in msg:
+    while connected:
+        chunk = commS.recv(4096).decode('utf-8')
+        if not chunk:
+            break
+        data += chunk
+
+        # Check if we have received the headers completely
+        if "\r\n\r\n" in data:
+            headers_body_split = data.split("\r\n\r\n", 1)
+            request_line, headers = headers_body_split[0].split("\r\n",1)
+            
+            method , uri, version = request_line.split(" ")
+            body = headers_body_split[1] if len(headers_body_split) > 1 else ""
+            
+            header_dict = parse_headers(headers)
+            
+            httpobj = HttpObj(method,uri,version)
+            httpobj.body = body
+            httpobj.headers_dict = header_dict
+            
+            # If there is a Content-Length header, read the specified amount of body data
+            if "Content-Length" in header_dict:
+                content_length = int(header_dict["Content-Length"])
+                while len(body) < content_length:
+                    body += commS.recv(4096).decode('utf-8')
+                
+            # print(f"Received HTTP request:\nHeaders: {headers}Body: {body}")
+            data = ""  # Reset data after processing the complete request
+            # Optionally handle the request here
+            fulfillRequest(httpobj,commS)
+            
+        else:
+            print("bad")
+            
+            
+    commS.close()
+            
+def fulfillRequest(httpObj, commS):
+    # Make the request using the `requests` library
+    try:
+        # Handling only GET requests for now
+        if httpObj.type == "GET":
+            response = requests.get(httpObj.target, headers=httpObj.headers_dict)
+        elif httpObj.type == "POST":
+            response = requests.post(httpObj.target, headers=httpObj.headers_dict, data=httpObj.body)
+        else:
+            # Handle unsupported methods
+            commS.send("HTTP/1.1 405 Method Not Allowed\r\n\r\n".encode())
+            return
         
-        request_line, headers_body = msg.split("\r\n", 1)
-        headers = headers_body.splitlines()
-        body = ""
-        header_dict = {}
+        # Construct HTTP response to send back to the client
+        server_response = f"HTTP/1.1 {response.status_code} {response.reason}\r\n"
+        for key, value in response.headers.items():
+            server_response += f"{key}: {value}\r\n"
+        server_response += "\r\n"  # End headers
+        server_response += response.text  # Add response body (HTML, JSON, etc.)
         
-        for i in range(len(headers)):
+        # Send the response back to the client
+        print(server_response)
+        commS.send(server_response.encode())
+    
+    except requests.exceptions.RequestException as e:
+        # If there was an error making the request, send an error response
+        error_response = f"HTTP/1.1 500 Internal Server Error\r\n\r\nError: {str(e)}"
+        commS.send(error_response.encode())
             
-            if(len(headers[i]) == 0):
-                body = headers[i+1]
-                break
-            
-        for header in headers:
-            if(len(header) == 0):
-                break
-            
-            key, value = header.split(": ",1)
-            
-            header_dict[key] = value
-            
-    else:
-        raise ValueError("Invalid HTTP request: Missing CRLF delimiter")
+def parse_headers(header_list):
+    header_dict = {}
+    header_list = header_list.split("\r\n")
     
-    method , uri , version = request_line.split(" ")
-    
-    httpObj = HttpObj(method,uri,version)
-    httpObj.headers_dict = header_dict
-    httpObj.body = body
-    
-    print(header_dict)
-    print(body)
-    
-    return httpObj
-    
-    
+    for header in header_list:
+        key, value = header.split(": ",1)   
+        header_dict[key] = value
+      
+    return header_dict
+  
 def start():
     s.listen(5)
     while True:
-        commS , address = s.accept
+        commS , address = s.accept()
 
         thread = threading.Thread(target = handle_client, args = (commS,address))
-        thread.start
-        print(f"active connections:{threading.activeCount() -1}")
+        thread.start()
+        
+        print(f"active connections:{threading.active_count() -1}")
 
-
-#print("starting server")
-testString = "POST /cgi-bin/process.cgi HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: www.tutorialspoint.com\r\n\
-Content-Type: application/x-www-form-urlencoded\r\nContent-Length: length\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\
-\r\n\r\nBlah blah"
-obj = parse_http(testString)
-
-#print(obj.type)
-#print(obj.target)
-#print(obj.version)
+print("starting server")
+start()
