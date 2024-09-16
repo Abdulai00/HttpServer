dce1b286a437bcb2e4b50e5fec57aef19079bb9d
Abdulai00 <abdulai.b0001@gmail.com>
2024-09-16 14:38:36 +0000

    I found this useful class that allows me to abstract the process of parsing strings and headers. so far I set up the bones of the connections inculding a part to handle http request. next step is to implement all the method required.

--- server.py
+++ server.py
@@ -1,26 +1,62 @@
+import http.server
+import socketserver
 import socket
-import ssl
 import threading
+import ssl
+import select
 import requests
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0",443))
+LISTENING_PORT = 8080  
+HTTPS_PORT = 443  
+CERT_FILE = 'placeholder'  
+KEY_FILE = 'placeholder'  
 
-PrivateKey = "/home/ec2-user/Server/HttpServer/bahproxy.com.key"
-Certification = "/home/ec2-user/Server/HttpServer/bahproxy.com.pem"
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile=Certification,keyfile=PrivateKey)
-  
-def start():
-    s.listen(5)
-    while True:
-        commS , address = s.accept()
-        tls_comms = context.wrap_socket(commS,server_side=True)
-        thread = threading.Thread(target = handle_client, args = (tls_comms,address))
-        thread.start()
-        
-        print(f"active connections:{threading.active_count() -1}")
+class ProxyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
 
-print("Listening for incoming traffic")
-start()
+    def do_GET(self):
+        self._handle_http_request()
+
+    def do_POST(self):
+        self._handle_http_request()
+
+    def do_PUT(self):
+        self._handle_http_request()
+
+    def do_DELETE(self):
+        self._handle_http_request()
+
+
+class ThreadingHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    daemon_threads = True
+
+def start_http_server():
+    """Start the HTTP proxy server."""
+    http_server = ThreadingHTTPServer(('', LISTENING_PORT), ProxyHTTPRequestHandler)
+    threading.Thread(target=http_server.serve_forever).start()
+    print(f"HTTP proxy server running on port {LISTENING_PORT}")
+
+
+def start_https_server():
+    """Start the HTTPS proxy server."""
+    https_server = ThreadingHTTPServer(('', HTTPS_PORT), ProxyHTTPRequestHandler)
+    https_server.socket = ssl.wrap_socket(
+        https_server.socket,
+        server_side=True,
+        certfile=CERT_FILE,
+        keyfile=KEY_FILE,
+        ssl_version=ssl.PROTOCOL_TLS
+    )
+    threading.Thread(target=https_server.serve_forever).start()
+    print(f"HTTPS proxy server running on port {HTTPS_PORT}")
+
+
+def start_proxy_server():
+    """Start both HTTP and HTTPS proxy servers."""
+    start_http_server()
+    start_https_server()
+
+
+if __name__ == "__main__":
+    start_proxy_server()
